8d7eead81d3c18db0e3e850b654ed0d7ec64beb2
Harold Velez <haroldvelez113@gmail.com>
2026-03-18 15:21:17 -0500

    feat: Add Basic nats client

--- shared/go.mod
+++ shared/go.mod
@@ -2,4 +2,15 @@ module github.com/HaroldVelez13/go_workers/shared
 
 go 1.26.1
 
-require github.com/google/uuid v1.6.0
+require (
+	github.com/google/uuid v1.6.0
+	github.com/nats-io/nats.go v1.49.0
+)
+
+require (
+	github.com/klauspost/compress v1.18.2 // indirect
+	github.com/nats-io/nkeys v0.4.12 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
+	golang.org/x/crypto v0.46.0 // indirect
+	golang.org/x/sys v0.39.0 // indirect
+)
---
+++ shared/nats/client.go
@@ -0,0 +1,33 @@
+package nats
+
+import (
+	"log"
+	"time"
+
+	"github.com/nats-io/nats.go"
+)
+
+type Client struct {
+	conn *nats.Conn
+}
+
+func NewClient(cfg Config) (*Client, error) {
+	conn, err := nats.Connect(
+		cfg.URL,
+		nats.Name("go-service"),
+		nats.ReconnectWait(2*time.Second),
+		nats.MaxReconnects(-1),
+		nats.DisconnectErrHandler(func(nc *nats.Conn, err error) {
+			log.Println("NATS disconnected:", err)
+		}),
+		nats.ReconnectHandler(func(nc *nats.Conn) {
+			log.Println("NATS reconnected")
+		}),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{conn: conn}, nil
+}
